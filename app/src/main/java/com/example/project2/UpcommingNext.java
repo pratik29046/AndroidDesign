785d8c3e3be2011456b3203480b10155f82a09b0
pratik29046 <pratikpawade8667@gmail.com>
2022-06-24 10:07:56 +0530

    Add new activity_upcomming_next.xml

---
+++ app/src/main/java/com/example/project2/UpcommingNext.java
@@ -0,0 +1,16 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpcommingNext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upcomming_next);
+
+
+    }
+}
